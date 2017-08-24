5105addfc661528949a1757c0d8b30878b724b9c
Dmitry Kulagin <kulaginds@gmail.com>
2017-08-24 20:00:40 +0300

    fixed: removed no.gif for photo; fixed: if photo dir not exists create it

--- controllers/home.php
+++ controllers/home.php
@@ -120,6 +120,11 @@ class HomeController extends AbstractController
 	protected function moveUploadedFile(UploadedFile $uploadedFile)
 	{
 		$directory = $this->container->get('upload_directory');
+
+		if (false === is_dir($directory)) {
+			mkdir($directory);
+		}
+
 		$extension = pathinfo($uploadedFile->getClientFilename(), PATHINFO_EXTENSION);
 		$basename  = bin2hex(random_bytes(8)); // see http://php.net/manual/en/function.random-bytes.php
 		$filename  = sprintf('%s.%0.8s', $basename, $extension);
